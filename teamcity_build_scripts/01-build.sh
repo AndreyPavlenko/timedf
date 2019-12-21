1139ef9747e69c764adc2adabfcb44f87da35d49
amyskov <55585026+amyskov@users.noreply.github.com>
2019-12-21 16:58:37 +0300

    minor script correction

--- teamcity_build_scripts/01-build.sh
+++ teamcity_build_scripts/01-build.sh
@@ -10,4 +10,8 @@ mkdir data
 ./bin/initdb --data data
 
 mkdir ibis
+cd ibis
 git clone https://github.com/ibis-project/ibis.git ./ibis
+pip install -r requirements.txt
+python3 setup.py build
+cd ..
