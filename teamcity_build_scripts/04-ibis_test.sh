28c2b5e6976f779316368f5fa15d6099a31495cf
Emelianova Svetlana <svetlana.emelianova@intel.com>
2020-01-28 13:19:22 +0300

    Added ibis in teamcity CI

---
+++ teamcity_build_scripts/04-ibis_test.sh
@@ -0,0 +1,1 @@
+python3 run_ibis_tests.py --env_name ibis-test --env_check True --save_env True --python_version 3.7 --task test --name agent_test_ibis --report "${PWD}"/.. --ibis_path "${PWD}"/../ibis/ --executable "${PWD}"/../omniscidb/build/bin/omnisci_server --user admin --password HyperInteractive -commit_omnisci ${BUILD_REVISION} -commit_ibis ${BUILD_IBIS_REVISION}
