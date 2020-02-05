48e42ed793b419500405ab2eabecf51e15c46425
Emelianova Svetlana <svetlana.emelianova@intel.com>
2020-02-05 19:47:43 +0300

    Updated build scripts

--- teamcity_build_scripts/02-build_ibis.sh
+++ teamcity_build_scripts/02-build_ibis.sh
@@ -1,1 +1,1 @@
-python3 run_ibis_tests.py --env_name ibis-test --env_check False --save_env True --python_version 3.7 --task build --name agent_test_ibis --ci_requirements "${PWD}"/ci_requirements.yml --ibis_path $PWD/../ibis/ --executable $PWD/../omniscidb/build/bin/omnisci_server
+python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check False --save_env True --python_version 3.7 --task build --name agent_test_ibis --ci_requirements "${PWD}"/ci_requirements.yml --ibis_path $PWD/../ibis/ --executable $PWD/../omniscidb/build/bin/omnisci_server
