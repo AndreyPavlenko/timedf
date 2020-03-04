76c710a8cccc3b38884cb1d649faf6b803341dbe
Emelianova Svetlana <svetlana.emelianova@intel.com>
2020-03-04 13:54:10 +0300

    Added encoding env variables

--- run_ibis_tests.py
+++ run_ibis_tests.py
@@ -100,6 +100,8 @@ try:
     os.environ["IBIS_TEST_OMNISCIDB_DATABASE"] = args.name
     os.environ["IBIS_TEST_DATA_DB"] = args.name
     os.environ["IBIS_TEST_OMNISCIDB_PORT"] = str(args.omnisci_port)
+    os.environ["PYTHONIOENCODING"] = 'UTF-8'
+    os.environ["PYTHONUNBUFFERED"] = '1'
 
     required_tasks = args.task.split(',')
     tasks = {}
