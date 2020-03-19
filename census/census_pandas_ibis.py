c874b97474c3d80b14763d575920536b2585726e
Emelianova Svetlana <svetlana.emelianova@intel.com>
2020-03-19 16:06:29 +0300

    Census benchmark refactoring

--- census/census_pandas_ibis.py
+++ census/census_pandas_ibis.py
@@ -1,43 +1,30 @@
 # coding: utf-8
 import argparse
 import gzip
+import json
 import os
 import sys
 import time
+import traceback
 import warnings
 from timeit import default_timer as timer
 
-import mysql.connector
+import cloudpickle
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
-from report import DbReport
 from server import OmnisciServer
-from utils import compare_dataframes, import_pandas_into_module_namespace
+from utils import (
+    compare_dataframes,
+    import_pandas_into_module_namespace,
+    load_data_pandas,
+    print_times,
+)
 
 warnings.filterwarnings("ignore")
 
 
 # Dataset link
 # https://rapidsai-data.s3.us-east-2.amazonaws.com/datasets/ipums_education2income_1970-2010.csv.gz
-def load_data(
-    filename,
-    columns_names=None,
-    columns_types=None,
-    header=None,
-    nrows=None,
-    use_gzip=False,
-):
-    types = None
-    if columns_types:
-        types = {columns_names[i]: columns_types[i] for i in range(len(columns_names))}
-    return pd.read_csv(
-        filename,
-        names=columns_names,
-        nrows=nrows,
-        header=header,
-        dtype=types,
-        compression="gzip" if use_gzip else None,
-    )
 
 
 def etl_pandas(filename, columns_names, columns_types):
@@ -52,12 +39,14 @@ def etl_pandas(filename, columns_names, columns_types):
     }
 
     t0 = timer()
-    df = load_data(
+    df = load_data_pandas(
         filename=filename,
         columns_names=columns_names,
         columns_types=columns_types,
         header=0,
         nrows=None,
+        use_gzip=filename.endswith(".gz"),
+        pd=main.__globals__["pd"],
     )
     etl_times["t_readcsv"] = timer() - t0
 
@@ -134,7 +123,6 @@ def etl_ibis(
     create_new_table,
     validation,
 ):
-
     etl_times = {
         "t_readcsv": 0.0,
         "t_where": 0.0,
@@ -157,7 +145,7 @@ def etl_ibis(
 
     t0 = timer()
 
-    omnisci_server_worker.connect_to_server()
+    omnisci_server_worker.connect_to_server(database=database_name)
     # Create table and import data
     if create_new_table:
         # Datafiles import
@@ -256,24 +244,6 @@ def etl_ibis(
     return df, X, y, etl_times
 
 
-def print_times(etl_times, backend, db_reporter=None):
-    print(f"{backend} times:")
-    for time_name, time in etl_times.items():
-        print("{} = {:.5f} s".format(time_name, time))
-        if db_reporter is not None:
-            db_reporter.submit(
-                {
-                    "QueryName": time_name,
-                    "FirstExecTimeMS": time * 1000,
-                    "WorstExecTimeMS": time * 1000,
-                    "BestExecTimeMS": time * 1000,
-                    "AverageExecTimeMS": time * 1000,
-                    "TotalTimeMS": time * 1000,
-                    "BackEnd": backend,
-                }
-            )
-
-
 def mse(y_test, y_pred):
     return ((y_test - y_pred) ** 2).mean()
 
@@ -348,26 +318,62 @@ def main():
     args = None
     omnisci_server_worker = None
 
-    parser = argparse.ArgumentParser(description="Run internal tests from ibis project")
+    parser = argparse.ArgumentParser(
+        description="Run cencus benchmark on Ibis and Pandas."
+    )
     optional = parser._action_groups.pop()
     required = parser.add_argument_group("required arguments")
     parser._action_groups.append(optional)
 
     required.add_argument(
-        "-f",
-        "--file",
-        dest="file",
-        required=True,
-        help="A datafile that should be loaded",
+        "-f", "--file", dest="file", help="A datafile that should be loaded.",
     )
+    optional.add_argument(
+        "-df",
+        "--dfiles_num",
+        dest="dfiles_num",
+        default=1,
+        type=int,
+        help="Number of datafiles to input into database for processing.",
+    )
+    required.add_argument(
+        "--omnisci_server_worker",
+        dest="omnisci_server_worker",
+        default="server_worker.pickled",
+        help="File with pickled omnisci_server_worker representation.",
+    )
+    optional.add_argument(
+        "--result_file",
+        dest="result_file",
+        default="taxi_results.json",
+        help="File to which the results will be written.",
+    )
+    # Omnisci server parameters
+    optional.add_argument(
+        "-db",
+        "--database_name",
+        dest="database_name",
+        default="omnisci",
+        help="Database name to use in omniscidb server.",
+    )
+    optional.add_argument(
+        "-t",
+        "--table",
+        dest="table",
+        default="benchmark_table",
+        help="Table name name to use in omniscidb server.",
+    )
+    # Ibis parameters
     optional.add_argument("-dnd", action="store_true", help="Do not delete old table.")
     optional.add_argument(
         "-dni",
         action="store_true",
         help="Do not create new table and import any data from CSV files.",
     )
+    # Benchmark parameters
     optional.add_argument(
         "-val",
+        dest="validation",
         action="store_true",
         help="validate queries results (by comparison with Pandas queries results).",
     )
@@ -379,110 +385,7 @@ def main():
         default="intel",
         help="Which optimizer is used",
     )
-    # MySQL database parameters
-    optional.add_argument(
-        "-db-server",
-        dest="db_server",
-        default="localhost",
-        help="Host name of MySQL server.",
-    )
-    optional.add_argument(
-        "-db-port",
-        dest="db_port",
-        default=3306,
-        type=int,
-        help="Port number of MySQL server.",
-    )
-    optional.add_argument(
-        "-db-user",
-        dest="db_user",
-        default="",
-        help="Username to use to connect to MySQL database. "
-        "If user name is specified, script attempts to store results in MySQL "
-        "database using other -db-* parameters.",
-    )
-    optional.add_argument(
-        "-db-pass",
-        dest="db_pass",
-        default="omniscidb",
-        help="Password to use to connect to MySQL database.",
-    )
-    optional.add_argument(
-        "-db-name",
-        dest="db_name",
-        default="omniscidb",
-        help="MySQL database to use to store benchmark results.",
-    )
-    optional.add_argument(
-        "-db-table",
-        dest="db_table",
-        help="Table to use to store results for this benchmark.",
-    )
-    # Omnisci server parameters
-    optional.add_argument(
-        "-e",
-        "--executable",
-        dest="omnisci_executable",
-        required=False,
-        help="Path to omnisci_server executable.",
-    )
-    optional.add_argument(
-        "-w",
-        "--workdir",
-        dest="omnisci_cwd",
-        help="Path to omnisci working directory. "
-        "By default parent directory of executable location is used. "
-        "Data directory is used in this location.",
-    )
-    optional.add_argument(
-        "-port",
-        "--omnisci_port",
-        dest="omnisci_port",
-        default=6274,
-        type=int,
-        help="TCP port number to run omnisci_server on.",
-    )
-    optional.add_argument(
-        "-u",
-        "--user",
-        dest="user",
-        default="admin",
-        help="User name to use on omniscidb server.",
-    )
-    optional.add_argument(
-        "-p",
-        "--password",
-        dest="password",
-        default="HyperInteractive",
-        help="User password to use on omniscidb server.",
-    )
-    optional.add_argument(
-        "-n",
-        "--name",
-        dest="name",
-        default="census_database",
-        help="Database name to use in omniscidb server.",
-    )
-    optional.add_argument(
-        "-t",
-        "--table",
-        dest="table",
-        default="census_table",
-        help="Table name name to use in omniscidb server.",
-    )
-
-    optional.add_argument(
-        "-commit_omnisci",
-        dest="commit_omnisci",
-        default="1234567890123456789012345678901234567890",
-        help="Omnisci commit hash to use for benchmark.",
-    )
-    optional.add_argument(
-        "-commit_ibis",
-        dest="commit_ibis",
-        default="1234567890123456789012345678901234567890",
-        help="Ibis commit hash to use for benchmark.",
-    )
+    # Benchmark parameters
     optional.add_argument(
         "-no_ibis",
         action="store_true",
@@ -509,8 +412,24 @@ def main():
         action="store_true",
         help="Do not run machine learning benchmark, only ETL part",
     )
+    optional.add_argument(
+        "-q3_full",
+        action="store_true",
+        help="Execute q3 query correctly (script execution time will be increased).",
+    )
 
     args = parser.parse_args()
+
+    ignored_args = {
+        "q3_full": args.q3_full,
+        "dfiles_num": args.dfiles_num,
+    }
+    if args.no_ibis:
+        ignored_args["omnisci_server_worker"] = args.omnisci_server_worker
+        ignored_args["dnd"] = args.dnd
+        ignored_args["dni"] = args.dni
+    warnings.warn(f"Parameters {ignored_args} are irnored", RuntimeWarning)
+
     args.file = args.file.replace("'", "")
 
     # ML specific
@@ -616,59 +535,25 @@ def main():
     db_reporter = None
 
     try:
+
+        import_pandas_into_module_namespace(
+            namespace=main.__globals__,
+            mode=args.pandas_mode,
+            ray_tmpdir=args.ray_tmpdir,
+            ray_memory=args.ray_memory,
+        )
+
+        etl_times_ibis = None
         if not args.no_ibis:
-            if args.omnisci_executable is None:
-                parser.error(
-                    "Omnisci executable should be specified with -e/--executable"
-                )
-            omnisci_server = OmnisciServer(
-                omnisci_executable=args.omnisci_executable,
-                omnisci_port=args.omnisci_port,
-                database_name=args.name,
-                user=args.user,
-                password=args.password,
-            )
-            omnisci_server.launch()
-            from server_worker import OmnisciServerWorker
-
-            omnisci_server_worker = OmnisciServerWorker(omnisci_server)
-
-            if args.db_user is not "":
-                print("Connecting to database")
-                db = mysql.connector.connect(
-                    host=args.db_server,
-                    port=args.db_port,
-                    user=args.db_user,
-                    passwd=args.db_pass,
-                    db=args.db_name,
-                )
-                db_reporter = DbReport(
-                    db,
-                    args.db_table,
-                    {
-                        "QueryName": "VARCHAR(500) NOT NULL",
-                        "FirstExecTimeMS": "BIGINT UNSIGNED",
-                        "WorstExecTimeMS": "BIGINT UNSIGNED",
-                        "BestExecTimeMS": "BIGINT UNSIGNED",
-                        "AverageExecTimeMS": "BIGINT UNSIGNED",
-                        "TotalTimeMS": "BIGINT UNSIGNED",
-                        "IbisCommitHash": "VARCHAR(500) NOT NULL",
-                        "BackEnd": "VARCHAR(100) NOT NULL",
-                    },
-                    {
-                        "ScriptName": "census_pandas_ibis.py",
-                        "CommitHash": args.commit_omnisci,
-                        "IbisCommitHash": args.commit_ibis,
-                    },
-                )
-
-            df_ibis, X_ibis, y_ibis, etl_times_ibis = etl_ibis(
+            X_ibis, y_ibis, etl_times_ibis = etl_ibis(
                 filename=args.file,
                 columns_names=columns_names,
                 columns_types=columns_types,
-                database_name=args.name,
+                database_name=args.database_name,
                 table_name=args.table,
-                omnisci_server_worker=omnisci_server_worker,
+                omnisci_server_worker=cloudpickle.load(
+                    open(args.omnisci_server_worker, "rb")
+                ),
                 delete_old_database=not args.dnd,
                 create_new_table=not args.dni,
                 validation=args.val,
@@ -687,13 +572,12 @@ def main():
                 print("mean MSE ± deviation: {:.9f} ± {:.9f}".format(mse_mean, mse_dev))
                 print("mean COD ± deviation: {:.9f} ± {:.9f}".format(cod_mean, cod_dev))
 
-        import_pandas_into_module_namespace(
-            main.__globals__, args.pandas_mode, args.ray_tmpdir, args.ray_memory
-        )
-        df, X, y, etl_times = etl_pandas(
+        X, y, etl_times = etl_pandas(
             args.file, columns_names=columns_names, columns_types=columns_types
         )
-        print_times(etl_times, args.pandas_mode, db_reporter)
+
+        print_times(etl_times=etl_times, backend=args.pandas_mode)
+        etl_times["Backend"] = args.pandas_mode
 
         if not args.no_ml:
             mse_mean, cod_mean, mse_dev, cod_dev, ml_times = ml(
@@ -703,10 +587,14 @@ def main():
             print("mean MSE ± deviation: {:.9f} ± {:.9f}".format(mse_mean, mse_dev))
             print("mean COD ± deviation: {:.9f} ± {:.9f}".format(cod_mean, cod_dev))
 
-        if args.val:
-            compare_dataframes((df_ibis,), (df,))
-    except Exception as err:
-        print("Failed: ", err)
+        if args.validation:
+            compare_dataframes(ibis_df=(X_ibis, y_ibis), pandas_df=(X, y),
+                               pd=main.__globals__["pd"])
+
+        with open(args.result_file, "w") as json_file:
+            json.dump([etl_times_ibis, etl_times], json_file)
+    except Exception:
+        traceback.print_exc(file=sys.stdout)
         sys.exit(1)
     finally:
         if omnisci_server_worker:
