ac25317097a01e8c010b6f8a50acb7afe2aa3e29
Emelianova Svetlana <svetlana.emelianova@intel.com>
2020-03-27 15:07:03 +0300

    Updated command line examples

--- teamcity_build_scripts/14-ny_taxi_pandas_ibis.sh
+++ teamcity_build_scripts/14-ny_taxi_pandas_ibis.sh
@@ -1,1 +1,10 @@
-python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check True --python_version 3.7 --task benchmark --ci_requirements "${PWD}"/ci_requirements.yml --save_env True --report "${PWD}"/.. --ibis_path "${PWD}"/../ibis/ --executable "${PWD}"/../omniscidb/build/bin/omnisci_server -u admin -p HyperInteractive -n ${DATABASE_NAME} --bench_name ny_taxi --dfiles_num 20 --dpattern '/localdisk/benchmark_datasets/taxi/trips_xa{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t}.csv.gz' --iters 5 -db-server ansatlin07.an.intel.com -db-port 3306 -db-user gashiman -db-pass omniscidb -db-name omniscidb -db-table taxibench_ibis -commit_omnisci ${BUILD_REVISION} -commit_ibis ${BUILD_IBIS_REVISION}
+python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check True --python_version 3.7                      \
+--task benchmark --ci_requirements "${PWD}"/ci_requirements.yml --save_env True --ibis_path                 \
+"${PWD}"/../ibis/ --executable "${PWD}"/../omniscidb/build/bin/omnisci_server                               \
+-calcite_port 62225 -http_port 1718 -port 1717 -user admin -password HyperInteractive                       \
+-database_name ${DATABASE_NAME} -table taxi -bench_name taxi -dfiles_num 20 -iterations 5                   \
+-data_file '/localdisk/benchmark_datasets/taxi/trips_xa{a,b,c,d,e,f,g,h,i,j,k,l,m,n,o,p,q,r,s,t}.csv.gz'    \
+-pandas_mode Pandas -ray_tmpdir /tmp -validation True                                                       \
+-db_server ansatlin07.an.intel.com -db_port 3306 -db_user gashiman -db_pass omniscidb -db_name omniscidb    \
+-db_table taxi_pandas_ibis                                                                                  \
+-commit_omnisci ${BUILD_REVISION} -commit_ibis ${BUILD_IBIS_REVISION}
