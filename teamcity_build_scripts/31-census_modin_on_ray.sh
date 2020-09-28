3e7ed701fc3863a5470ca882bd049e94ffb4017c
Alexander Myskov <alexander.myskov@intel.com>
2020-09-28 07:48:13 -0500

    update scripts for Census benchmark

--- teamcity_build_scripts/31-census_modin_on_ray.sh
+++ teamcity_build_scripts/31-census_modin_on_ray.sh
@@ -2,11 +2,9 @@
 
 mkdir -p ${PWD}/tmp
 python3 run_ibis_tests.py --env_name ${ENV_NAME} --env_check True --save_env True --python_version 3.7 -task benchmark         \
-                          --ci_requirements "${PWD}/ci_requirements.yml"                                                       \
-                          -executable "${PWD}/../omniscidb/build/bin/omnisci_server"                                           \
-                          -table census -bench_name census -dfiles_num 1 -iterations 1                                         \
+                          --ci_requirements "${PWD}/ci_requirements.yml" -bench_name census                                    \
                           -data_file '${DATASETS_PWD}/census/ipums_education2income_1970-2010.csv.gz'                          \
-                          -pandas_mode Modin_on_ray -ray_tmpdir /tmp                                                           \
+                          -pandas_mode Modin_on_ray -ray_tmpdir ${PWD}/tmp                                                     \
                           -commit_omnisci ${BUILD_REVISION}                                                                    \
                           -commit_omniscripts ${BUILD_OMNISCRIPTS_REVISION}                                                    \
                           -commit_modin ${BUILD_MODIN_REVISION}                                                                \
