8cff79caeab454e077c41ea7e0fd0eb4db6091b8
Artem Alekseev <artem.alekseev@intel.com>
2020-11-23 14:49:23 +0300

    Add tpc-h to bearysta benches (#187)

---
+++ bearysta-scripts/db-query-list-tpc-h.sql
@@ -0,0 +1,149 @@
+USER admin omnisci {
+
+CREATE DATAFRAME NATION  ( N_NATIONKEY  INTEGER NOT NULL,
+                            N_NAME       CHAR(25) NOT NULL,
+                            N_REGIONKEY  INTEGER NOT NULL,
+                            N_COMMENT    VARCHAR(152)) from 'CSV:nation.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+CREATE DATAFRAME REGION  ( R_REGIONKEY  INTEGER NOT NULL,
+                            R_NAME       CHAR(25) NOT NULL,
+                            R_COMMENT    VARCHAR(152)) from 'CSV:region.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+CREATE DATAFRAME PART  ( P_PARTKEY     INTEGER NOT NULL,
+                          P_NAME        VARCHAR(55) NOT NULL,
+                          P_MFGR        CHAR(25) NOT NULL,
+                          P_BRAND       CHAR(10) NOT NULL,
+                          P_TYPE        VARCHAR(25) NOT NULL,
+                          P_SIZE        INTEGER NOT NULL,
+                          P_CONTAINER   CHAR(10) NOT NULL,
+                          P_RETAILPRICE DECIMAL(15,2) NOT NULL,
+                          P_COMMENT     VARCHAR(23) NOT NULL ) from 'CSV:part.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+
+CREATE DATAFRAME SUPPLIER ( S_SUPPKEY     INTEGER NOT NULL,
+                             S_NAME        CHAR(25) NOT NULL,
+                             S_ADDRESS     VARCHAR(40) NOT NULL,
+                             S_NATIONKEY   INTEGER NOT NULL,
+                             S_PHONE       CHAR(15) NOT NULL,
+                             S_ACCTBAL     DECIMAL(15,2) NOT NULL,
+                             S_COMMENT     VARCHAR(101) NOT NULL) from 'CSV:supplier.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+
+CREATE DATAFRAME PARTSUPP ( PS_PARTKEY     INTEGER NOT NULL,
+                             PS_SUPPKEY     INTEGER NOT NULL,
+                             PS_AVAILQTY    INTEGER NOT NULL,
+                             PS_SUPPLYCOST  DECIMAL(15,2)  NOT NULL,
+                             PS_COMMENT     VARCHAR(199) NOT NULL )   from 'CSV:partsupp.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+CREATE DATAFRAME CUSTOMER ( C_CUSTKEY     INTEGER NOT NULL,
+                             C_NAME        VARCHAR(25) NOT NULL,
+                             C_ADDRESS     VARCHAR(40) NOT NULL,
+                             C_NATIONKEY   INTEGER NOT NULL,
+                             C_PHONE       CHAR(15) NOT NULL,
+                             C_ACCTBAL     DECIMAL(15,2)   NOT NULL,
+                             C_MKTSEGMENT  CHAR(10) NOT NULL,
+                             C_COMMENT     VARCHAR(117) NOT NULL) from 'CSV:customer.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+CREATE DATAFRAME ORDERS  ( O_ORDERKEY       INTEGER NOT NULL,
+                           O_CUSTKEY        INTEGER NOT NULL,
+                           O_ORDERSTATUS    CHAR(1) NOT NULL,
+                           O_TOTALPRICE     DECIMAL(15,2) NOT NULL,
+                           O_ORDERDATE      TIMESTAMP NOT NULL,
+                           O_ORDERPRIORITY  CHAR(15) NOT NULL,  
+                           O_CLERK          CHAR(15) NOT NULL, 
+                           O_SHIPPRIORITY   INTEGER NOT NULL,
+                           O_COMMENT        VARCHAR(79) NOT NULL) from 'CSV:orders.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+CREATE DATAFRAME LINEITEM ( L_ORDERKEY    INTEGER NOT NULL,
+                             L_PARTKEY     INTEGER NOT NULL,
+                             L_SUPPKEY     INTEGER NOT NULL,
+                             L_LINENUMBER  INTEGER NOT NULL,
+                             L_QUANTITY    DECIMAL(15,2) NOT NULL,
+                             L_EXTENDEDPRICE  DECIMAL(15,2) NOT NULL,
+                             L_DISCOUNT    DECIMAL(15,2) NOT NULL,
+                             L_TAX         DECIMAL(15,2) NOT NULL,
+                             L_RETURNFLAG  CHAR(1) NOT NULL,
+                             L_LINESTATUS  CHAR(1) NOT NULL,
+                             L_SHIPDATE    TIMESTAMP NOT NULL,
+                             L_COMMITDATE  TIMESTAMP NOT NULL,
+                             L_RECEIPTDATE TIMESTAMP NOT NULL,
+                             L_SHIPINSTRUCT CHAR(25) NOT NULL,
+                             L_SHIPMODE     CHAR(10) NOT NULL,
+                             L_COMMENT      VARCHAR(44) NOT NULL) from 'CSV:lineitem.tbl' WITH (FRAGMENT_SIZE=5000000, header='false', delimiter='|');
+
+select
+	l_returnflag,
+	l_linestatus,
+	sum(l_quantity) as sum_qty,
+	sum(l_extendedprice) as sum_base_price,
+	sum(l_extendedprice * (1 - l_discount)) as sum_disc_price,
+	sum(l_extendedprice * (1 - l_discount) * (1 + l_tax)) as sum_charge,
+	avg(l_quantity) as avg_qty,
+	avg(l_extendedprice) as avg_price,
+	avg(l_discount) as avg_disc,
+	count(*) as count_order
+from
+	lineitem
+where
+	l_shipdate <= date '1998-12-01' - interval '97' day (3)
+group by
+	l_returnflag,
+	l_linestatus
+order by
+	l_returnflag,
+	l_linestatus;
+
+select
+	n_name,
+	sum(l_extendedprice * (1 - l_discount)) as revenue
+from
+	customer,
+	orders,
+	lineitem,
+	supplier,
+	nation,
+	region
+where
+	c_custkey = o_custkey
+	and l_orderkey = o_orderkey
+	and l_suppkey = s_suppkey
+	and c_nationkey = s_nationkey
+	and s_nationkey = n_nationkey
+	and n_regionkey = r_regionkey
+	and r_name = 'AFRICA'
+	and o_orderdate >= date '1995-01-01'
+	and o_orderdate < date '1995-01-01' + interval '1' year
+group by
+	n_name
+order by
+	revenue desc;
+
+select
+	ps_partkey,
+	sum(ps_supplycost * ps_availqty) as val
+from
+	partsupp,
+	supplier,
+	nation
+where
+	ps_suppkey = s_suppkey
+	and s_nationkey = n_nationkey
+	and n_name = 'UNITED STATES'
+group by
+	ps_partkey having
+		sum(ps_supplycost * ps_availqty) > (
+			select
+				sum(ps_supplycost * ps_availqty) * 0.0000033333
+			from
+				partsupp,
+				supplier,
+				nation
+			where
+				ps_suppkey = s_suppkey
+				and s_nationkey = n_nationkey
+				and n_name = 'UNITED STATES'
+		)
+order by
+	val desc;
+
+}
--- bearysta-scripts/run-omnisci.sh
+++ bearysta-scripts/run-omnisci.sh
@@ -2,4 +2,4 @@
 set -xe
 # replacing string like "storage_type='CSV:trips-header.csv', fragment_size=5000000);"
 sed -ri.bak -e "s/trips.+\.csv/trips-header-$rows.csv/; s/fragment_size=[0-9]+/fragment_size=$frags/" db-query-list-taxi.sql
-numactl $numa -C $cpus ../../build/bin/omnisci_server --config omnisci-bench-taxi.conf 2>&1
+numactl $numa -C $cpus $omnisci_server --config omnisci-bench-taxi.conf 2>&1
