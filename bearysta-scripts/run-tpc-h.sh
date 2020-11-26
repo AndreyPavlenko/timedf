0e65fc95edb2027fc872fbacdb50e48154cce2fe
Artem Alekseev <artem.alekseev@intel.com>
2020-11-26 16:04:02 +0300

    Add tpc-h query generation (#188)

--- bearysta-scripts/run-tpc-h.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -xe
-# replacing string like fragment_size=5000000);"
-sed -ri.bak -e "s/fragment_size=[0-9]+/fragment_size=$frags/" db-query-list-tpc-h.sql
-numactl $numa -C $cpus $omnisci_server --config omnisci-bench-tpc-h.conf 2>&1
