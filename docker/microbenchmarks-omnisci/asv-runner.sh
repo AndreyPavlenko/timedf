28c3274375a99283450344efeac1e11ab01b452f
Anatoly Myachev <anatoly.myachev@intel.com>
2021-07-08 22:31:00 +0300

    use report_asv_result.py in asv-runner.sh
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- docker/microbenchmarks-omnisci/asv-runner.sh
+++ docker/microbenchmarks-omnisci/asv-runner.sh
@@ -11,4 +11,7 @@ asv run  --launch-method=forkserver --config asv.conf.omnisci.json \
     -b omnisci.benchmarks.TimeAppend --machine docker-omnisci -a repeat=3 \
     --show-stderr --python=same
 
-# run scripts_db_deploy.sh
+cd ../..
+python omniscripts/report_asv_result.py \
+    --result-path modin/asv_bench/.asv/results/<machine-name>/<json-name> \
+    ${DB_COMMON_OPTS}
