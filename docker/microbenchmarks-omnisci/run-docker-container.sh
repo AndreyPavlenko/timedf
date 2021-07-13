09ba6dc4806fcfc9f38193db39204da2f0d1e723
Anatoly Myachev <anatoly.myachev@intel.com>
2021-07-13 14:11:54 +0300

    remove debug code in run-docker-container.sh
    Signed-off-by: Anatoly Myachev <anatoly.myachev@intel.com>

--- docker/microbenchmarks-omnisci/run-docker-container.sh
+++ docker/microbenchmarks-omnisci/run-docker-container.sh
@@ -16,13 +16,3 @@ fi
 
 docker run --shm-size $SHM_MEM --rm --name $CONTAINER_NAME $IMAGE_NAME \
     bash --login omniscripts/docker/microbenchmarks-omnisci/asv-runner.sh
-
-# docker run --rm --name $CONTAINER_NAME $IMAGE_NAME \
-#    bash --login -c "export MODIN_BACKEND=omnisci \
-# && export MODIN_EXPERIMENTAL=true \
-# && export MODIN_TEST_DATASET_SIZE=big \
-# && cd modin/asv_bench \
-# && conda activate modin_on_omnisci \
-# && asv run --launch-method=forkserver --config asv.conf.omnisci.json \
-#    -b omnisci.benchmarks.TimeAppend --machine docker-omnisci -a repeat=3 \
-#    --show-stderr --python=same"
