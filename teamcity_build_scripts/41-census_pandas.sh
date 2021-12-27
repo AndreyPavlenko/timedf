7b91226225f9ea526cf955d19e4a36d1da1aeb2a
Alexander Myskov <alexander.myskov@intel.com>
2021-12-27 19:26:32 +0300

    add pandas launches (#270)
    Signed-off-by: Alexander Myskov <alexander.myskov@intel.com>

---
+++ teamcity_build_scripts/41-census_pandas.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -e
+
+mkdir -p ${PWD}/tmp
+python3 run_modin_tests.py --env_name ${ENV_NAME} --env_check True --save_env True -task benchmark                             \
+                          -bench_name census                                                                                   \
+                          -data_file "${DATASETS_PWD}/census/ipums_education2income_1970-2010.csv.gz"                          \
+                          -pandas_mode Pandas -ray_tmpdir ${PWD}/tmp                                                           \
+                          ${ADDITIONAL_OPTS}                                                                                   \
+                          ${ADDITIONAL_OPTS_NIGHTLY}                                                                           \
+                          ${DB_COMMON_OPTS} ${DB_CENSUS_OPTS}
