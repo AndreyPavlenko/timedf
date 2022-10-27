d47f972a5eb99a6d15201e373e7a96456c338dd8
Egor Krivov <e.a.krivov@gmail.com>
2022-10-27 15:58:17 +0200

    Ray benchmarking, update asv-runner.sh (#309)
    Added benchmarking for ray

--- docker/microbenchmarks-hdk/asv-runner.sh
+++ docker/microbenchmarks-hdk/asv-runner.sh
@@ -1,10 +1,7 @@
 #!/bin/bash -e
-
 eval source ${CONDA_PREFIX}/bin/activate ${ENV_NAME}
 
-# get performance of modin on hdk
-export MODIN_ENGINE=native
-export MODIN_STORAGE_FORMAT=hdk
+# Basic config
 export MODIN_EXPERIMENTAL=true
 export MODIN_TEST_DATASET_SIZE=big
 export MODIN_ASV_DATASIZE_CONFIG=`pwd`/omniscripts/docker/microbenchmarks-hdk/modin-asv-datasize-config.json
@@ -13,11 +10,32 @@ cd modin/asv_bench
 
 # setup ASV machines
 asv machine --yes
+asv machine --machine ${HOST_NAME}-ray --yes
 asv machine --machine ${HOST_NAME}-hdk --yes
 asv machine --machine ${HOST_NAME}-pandas --yes
 
+RAY_MACHINE_NAME=$HOST_NAME-ray
 HDK_MACHINE_NAME=$HOST_NAME-hdk
 PANDAS_MACHINE_NAME=$HOST_NAME-pandas
+
+###############
+# Ray config ##
+###############
+
+export MODIN_ENGINE=ray
+
+asv run --launch-method=spawn --config asv.conf.hdk.json \
+    -b ^hdk --machine $RAY_MACHINE_NAME -a repeat=3 -a number=1 \
+    --show-stderr --python=same --set-commit-hash HEAD
+RAY_RESULT_NAME=`ls .asv/results/$RAY_MACHINE_NAME/ | grep existing`
+
+
+##############
+# HDK config #
+##############
+
+export MODIN_ENGINE=native
+export MODIN_STORAGE_FORMAT=hdk
 
 # 'number=1' allows for each bench run to get tables with different names (in `trigger_import`),
 # which eliminates caching inside HDK (`setup` is called before every run)
@@ -26,7 +44,10 @@ asv run --launch-method=forkserver --config asv.conf.hdk.json \
     --show-stderr --python=same --set-commit-hash HEAD
 HDK_RESULT_NAME=`ls .asv/results/$HDK_MACHINE_NAME/ | grep existing`
 
-# get performance of pure pandas
+################# 
+# Pandas config #
+#################
+
 export MODIN_ASV_USE_IMPL=pandas
 
 asv run --launch-method=forkserver --config asv.conf.hdk.json \
@@ -42,8 +63,13 @@ fi
 
 cd ../../omniscripts
 python report_asv_result.py \
+    --result-path ../modin/asv_bench/.asv/results/$RAY_MACHINE_NAME/$RAY_RESULT_NAME \
+    $DB_COMMON_OPTS
+
+python report_asv_result.py \
     --result-path ../modin/asv_bench/.asv/results/$HDK_MACHINE_NAME/$HDK_RESULT_NAME \
     $DB_COMMON_OPTS
+
 
 # report pandas results
 python report_asv_result.py \
