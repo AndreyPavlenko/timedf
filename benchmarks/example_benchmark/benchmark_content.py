dcf9df2a51aa9816f9de8871dbcc58eb0f550dd3
Egor Krivov <e.a.krivov@gmail.com>
2023-02-23 22:53:17 +0100

    Benchmark example (#369)

---
+++ benchmarks/example_benchmark/benchmark_content.py
@@ -0,0 +1,46 @@
+import numpy as np
+
+# No pandas is needed, it is imported on the next line
+# This is optional, this import will provide you with either pandas or modin, depending on run
+# params. If you not working with pandas API you have no point in using it.
+# However, if you are measuring pandas against pandas API library like modin you are advised to use
+# this tool.
+from omniscripts.pandas_backend import pd
+
+from omniscripts import BaseBenchmark, BenchmarkResults
+from omniscripts import TimerManager
+
+
+# This is an optional tool to simplity time tracking, without it you need to gather timing results 
+# manually into a simple dict (measurement: time_s) like {'load_data': 11.1, 'fe': 1.122}
+tm = TimerManager()
+
+
+class Benchmark(BaseBenchmark):
+    # This is optional to give user warning if he provided these arguments for some reason
+    __unsupported_params__ = ("optimizer", "dfiles_num")
+
+    def run_benchmark(self, params) -> BenchmarkResults:
+        N = 10_000
+
+        # This command will measure time it took to run nested block and record it with
+        # 'load_data' key
+        with tm.timeit("load_data"):
+            # your data path will be in params["data_file"]
+            # so you can use pd.read_csv(params["data_file"]) to read it
+            df = pd.DataFrame(np.random.randint(0, 100, size=(N, 4)), columns=list("abcd"))
+
+        with tm.timeit("fe"):
+            df["f"] = (df["a"] ** 2 + df["b"] ** 2) ** 0.5
+
+        with tm.timeit("evaluation"):
+            df["y"] = (df["a"] + df["c"] + df["d"] + df["f"]) / 4 > 0.5
+
+        # This will be a dict with timing results that look like:
+        # {'load_data': 1.1, 'fe': 11.1, 'evaluation': 1.12}
+        measurement2time = tm.get_results()
+        
+        # This is optional, you can pass arbitrary json data to save along with benchmark results
+        benchmark_specific_record = {'dataset_size': N}
+        # This is required result format
+        return BenchmarkResults(measurement2time, benchmark_specific_record)
