639e96f4114e63ad2bd93bbc056d78037daad2a2
Egor Krivov <e.a.krivov@gmail.com>
2023-04-04 14:50:39 +0200

    H2O refactored version (#388)

---
+++ build_scripts/h2o_ext.sh
@@ -0,0 +1,6 @@
+#!/bin/bash -e
+
+BENCH_NAME="h2o_ext"
+DATA_FILE="${DATASETS_PWD}/h2o/" 
+
+source $(dirname "$0")/00-run_bench.sh
--- omniscripts/pandas_backend.py
+++ omniscripts/pandas_backend.py
@@ -7,12 +7,13 @@ import pandas as pd  # noqa: F401 this import exists to provide vscode support f
 
 from .modin_utils import (
     import_pandas_into_module_namespace,
-    trigger_execution_base as _trigger_execution,
+    trigger_execution_base as _trigger_execution_pandas,
 )
 
 # Modin config, none if pandas is used
 # Variable will hold the state, used for `trigger_execution`
 modin_cfg = None
+backend_cfg = {}
 
 
 def _get_modin_config(pandas_mode):
@@ -26,6 +27,9 @@ def _get_modin_config(pandas_mode):
 
 def set_backend(pandas_mode, ray_tmpdir, ray_memory):
     global modin_cfg
+    global backend_cfg
+    backend_cfg["backend"] = pandas_mode
+
     modin_cfg = _get_modin_config(pandas_mode)
     import_pandas_into_module_namespace(
         namespace=globals(), mode=pandas_mode, ray_tmpdir=ray_tmpdir, ray_memory=ray_memory
@@ -34,4 +38,8 @@ def set_backend(pandas_mode, ray_tmpdir, ray_memory):
 
 def trigger_execution(*dfs):
     """Utility function to trigger execution for lazy pd libraries."""
-    return _trigger_execution(*dfs, modin_cfg=modin_cfg)
+    _trigger_execution_pandas(*dfs, modin_cfg=modin_cfg)
+    if len(dfs) == 1:
+        return dfs[0]
+    else:
+        return dfs
