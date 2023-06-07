74a649f6a3959efedb67bfacf7206d47dd0158e4
Arun Jose <40291569+arunjose696@users.noreply.github.com>
2023-06-07 12:59:48 +0200

    build_scripts now support mpiexec for unidist

--- build_scripts/00-run_bench.sh
+++ build_scripts/00-run_bench.sh
@@ -17,9 +17,20 @@ fi
 
 # ENV_NAME must be provided
 # live stream will provide live stdout and stderr
-conda run --live-stream -n $ENV_NAME benchmark-run $BENCH_NAME    \
+if [ ${PANDAS_MODE} = "Modin_on_unidist_mpi" ]; then
+    conda run --live-stream -n $ENV_NAME mpiexec -n 1 benchmark-run $BENCH_NAME    \
                            -data_file "${DATASETS_PWD}/${BENCH_NAME}" \
                            -backend ${PANDAS_MODE}            \
                            ${ADDITIONAL_OPTS}                     \
                            ${DB_COMMON_OPTS}                      \
                            "$@"
+else
+  conda run --live-stream -n $ENV_NAME benchmark-run $BENCH_NAME    \
+                           -data_file "${DATASETS_PWD}/${BENCH_NAME}" \
+                           -backend ${PANDAS_MODE}            \
+                           ${ADDITIONAL_OPTS}                     \
+                           ${DB_COMMON_OPTS}                      \
+                           "$@"
+
+fi
+
