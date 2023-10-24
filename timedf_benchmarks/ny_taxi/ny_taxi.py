4c08b054a2d1d6f9fce616f77f050b318e015185
Egor <e.a.krivov@gmail.com>
2023-10-24 13:27:00 +0200

    Updated ny_taxi to current timedf APIs (#531)
    * ny_taxi_ml is updated to new timedf APIs
    
    * Removed accidental double total
    
    * Moved to print_results
    
    * Updated ny_taxi
    
    * Reverted ny_taxi_ml
    
    * Added total time
    
    * fixed a bug

--- timedf_benchmarks/ny_taxi/ny_taxi.py
+++ timedf_benchmarks/ny_taxi/ny_taxi.py
@@ -3,9 +3,8 @@ import argparse
 from collections import OrderedDict
 from itertools import islice
 from pathlib import Path
-from timeit import default_timer as timer
 
-from timedf import BenchmarkResults, BaseBenchmark
+from timedf import BenchmarkResults, BaseBenchmark, tm
 from timedf.backend import pd, Backend
 from timedf.benchmark_utils import load_data_pandas, load_data_modin_on_hdk, print_results
 
@@ -37,24 +36,130 @@ ny_taxi_data_files_sizes_MB = OrderedDict(
     }
 )
 
+columns_names = [
+    "trip_id",
+    "vendor_id",
+    "pickup_datetime",
+    "dropoff_datetime",
+    "store_and_fwd_flag",
+    "rate_code_id",
+    "pickup_longitude",
+    "pickup_latitude",
+    "dropoff_longitude",
+    "dropoff_latitude",
+    "passenger_count",
+    "trip_distance",
+    "fare_amount",
+    "extra",
+    "mta_tax",
+    "tip_amount",
+    "tolls_amount",
+    "ehail_fee",
+    "improvement_surcharge",
+    "total_amount",
+    "payment_type",
+    "trip_type",
+    "pickup",
+    "dropoff",
+    "cab_type",
+    "precipitation",
+    "snow_depth",
+    "snowfall",
+    "max_temperature",
+    "min_temperature",
+    "average_wind_speed",
+    "pickup_nyct2010_gid",
+    "pickup_ctlabel",
+    "pickup_borocode",
+    "pickup_boroname",
+    "pickup_ct2010",
+    "pickup_boroct2010",
+    "pickup_cdeligibil",
+    "pickup_ntacode",
+    "pickup_ntaname",
+    "pickup_puma",
+    "dropoff_nyct2010_gid",
+    "dropoff_ctlabel",
+    "dropoff_borocode",
+    "dropoff_boroname",
+    "dropoff_ct2010",
+    "dropoff_boroct2010",
+    "dropoff_cdeligibil",
+    "dropoff_ntacode",
+    "dropoff_ntaname",
+    "dropoff_puma",
+]
+
+columns_types = [
+    "int64",
+    "category",
+    "timestamp",
+    "timestamp",
+    "category",
+    "int64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "int64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "category",
+    "float64",
+    "category",
+    "category",
+    "category",
+    "float64",
+    "int64",
+    "float64",
+    "int64",
+    "int64",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "category",
+    "float64",
+    "float64",
+    "category",
+    "category",
+    "category",
+    "float64",
+    "float64",
+    "float64",
+    "float64",
+    "category",
+    "float64",
+    "float64",
+    "category",
+    "category",
+    "category",
+    "float64",
+]
+
+
+def check_hdk():
+    return Backend.get_name() == "Modin_on_hdk"
+
 
 def get_ny_taxi_dataset_size(dfiles_num):
     return sum(list(ny_taxi_data_files_sizes_MB.values())[:dfiles_num])
 
 
-def run_queries(queries, parameters, etl_results, output_for_validation=None):
+def run_queries(queries, q2df, output_for_validation=None):
     for query_name, query_func in queries.items():
-        query_result = query_func(**parameters[query_name])
-        etl_results[query_name] = (
-            query_result[0]
-            if isinstance(query_result, (tuple, list)) and len(query_result) == 2
-            else query_result
-        )
+        with tm.timeit(query_name):
+            query_result = query_func(q2df[query_name])
         if output_for_validation is not None:
             assert len(query_result) == 2
             output_for_validation[query_name] = query_result[1]
-
-    return etl_results
 
 
 # Queries definitions
@@ -62,34 +167,30 @@ def run_queries(queries, parameters, etl_results, output_for_validation=None):
 #       count(*)
 # FROM trips
 # GROUP BY cab_type;
-def q1(df, backend):
-    t0 = timer()
-    if backend != "Modin_on_hdk":
+def q1(df):
+    if not check_hdk():
         q1_output = df.groupby("cab_type")["cab_type"].count()
     else:
         q1_output = df.groupby("cab_type").size()
         Backend.trigger_execution(q1_output)
-    query_time = timer() - t0
 
-    return query_time, q1_output
+    return q1_output
 
 
 # SELECT passenger_count,
 #       avg(total_amount)
 # FROM trips
 # GROUP BY passenger_count;
-def q2(df, backend):
-    t0 = timer()
-    if backend != "Modin_on_hdk":
+def q2(df):
+    if not check_hdk():
         q2_output = df.groupby("passenger_count", as_index=False).mean(numeric_only=True)[
             ["passenger_count", "total_amount"]
         ]
     else:
         q2_output = df.groupby("passenger_count").agg({"total_amount": "mean"})
         Backend.trigger_execution(q2_output)
-    query_time = timer() - t0
 
-    return query_time, q2_output
+    return q2_output
 
 
 # SELECT passenger_count,
@@ -98,9 +199,8 @@ def q2(df, backend):
 # FROM trips
 # GROUP BY passenger_count,
 #         pickup_year;
-def q3(df, backend):
-    t0 = timer()
-    if backend != "Modin_on_hdk":
+def q3(df):
+    if not check_hdk():
         transformed = pd.DataFrame(
             {
                 "pickup_datetime": df["pickup_datetime"].dt.year,
@@ -114,9 +214,8 @@ def q3(df, backend):
         df["pickup_datetime"] = df["pickup_datetime"].dt.year
         q3_output = df.groupby(["passenger_count", "pickup_datetime"]).size()
         Backend.trigger_execution(q3_output)  # to trigger real execution
-    query_time = timer() - t0
 
-    return query_time, q3_output
+    return q3_output
 
 
 # SELECT passenger_count,
@@ -141,9 +240,8 @@ def q3(df, backend):
 #         pickup_year,
 #         distance
 # ORDER BY passenger_count, pickup_year, distance, the_count;
-def q4(df, backend):
-    t0 = timer()
-    if backend != "Modin_on_hdk":
+def q4(df):
+    if not check_hdk():
         transformed = pd.DataFrame(
             {
                 "passenger_count": df["passenger_count"],
@@ -168,176 +266,56 @@ def q4(df, backend):
             .sort_values(by=["pickup_datetime", 0], ignore_index=True, ascending=[True, False])
         )
         Backend.trigger_execution(q4_output)  # to trigger real execution
-    query_time = timer() - t0
 
-    return query_time, q4_output
+    return q4_output
 
 
-def etl(filename, files_limit, columns_names, columns_types, output_for_validation, backend):
-    if backend == "Modin_on_hdk" and any(f.endswith(".gz") for f in filename):
+def etl(filename, columns_names, columns_types, output_for_validation):
+    if check_hdk() and any(f.endswith(".gz") for f in filename):
         raise NotImplementedError(
             "Modin_on_hdk mode doesn't support import of compressed files yet"
         )
 
-    etl_results = {}
-    t0 = timer()
-    if backend == "Modin_on_hdk":
-        df_from_each_file = [
-            load_data_modin_on_hdk(
-                filename=f,
-                columns_names=columns_names,
-                columns_types=columns_types,
-                parse_dates=["timestamp"],
-                pd=pd,
-            )
-            for f in filename
-        ]
-    else:
-        df_from_each_file = [
-            load_data_pandas(
-                filename=f,
-                columns_names=columns_names,
-                header=None,
-                nrows=None,
-                use_gzip=f.endswith(".gz"),
-                parse_dates=["pickup_datetime", "dropoff_datetime"],
-                pd=pd,
-            )
-            for f in filename
-        ]
+    with tm.timeit("t_readcsv"):
+        if check_hdk():
+            df_from_each_file = [
+                load_data_modin_on_hdk(
+                    filename=f,
+                    columns_names=columns_names,
+                    columns_types=columns_types,
+                    parse_dates=["timestamp"],
+                    pd=pd,
+                )
+                for f in filename
+            ]
+        else:
+            df_from_each_file = [
+                load_data_pandas(
+                    filename=f,
+                    columns_names=columns_names,
+                    header=None,
+                    nrows=None,
+                    use_gzip=f.endswith(".gz"),
+                    parse_dates=["pickup_datetime", "dropoff_datetime"],
+                    pd=pd,
+                )
+                for f in filename
+            ]
 
-    concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
-    Backend.trigger_loading(concatenated_df)
-    etl_results["t_readcsv"] = timer() - t0
+        concatenated_df = pd.concat(df_from_each_file, ignore_index=True)
+        Backend.trigger_loading(concatenated_df)
 
     queries = {"Query1": q1, "Query2": q2, "Query3": q3, "Query4": q4}
-    etl_results.update({q: 0.0 for q in queries})
-    queries_parameters = {
-        query_name: {
-            # FIXME seems like such copy op can affect benchmark
-            "df": concatenated_df.copy() if backend == "Modin_on_hdk" else concatenated_df,
-            "backend": backend,
-        }
+    q2df = {
+        # FIXME seems like such copy op can affect benchmark
+        query_name: concatenated_df.copy() if check_hdk() else concatenated_df
         for query_name in queries
     }
 
-    return run_queries(
-        queries=queries,
-        parameters=queries_parameters,
-        etl_results=etl_results,
-        output_for_validation=output_for_validation,
-    )
+    return run_queries(queries=queries, q2df=q2df, output_for_validation=output_for_validation)
 
 
 def run_benchmark(parameters):
-    columns_names = [
-        "trip_id",
-        "vendor_id",
-        "pickup_datetime",
-        "dropoff_datetime",
-        "store_and_fwd_flag",
-        "rate_code_id",
-        "pickup_longitude",
-        "pickup_latitude",
-        "dropoff_longitude",
-        "dropoff_latitude",
-        "passenger_count",
-        "trip_distance",
-        "fare_amount",
-        "extra",
-        "mta_tax",
-        "tip_amount",
-        "tolls_amount",
-        "ehail_fee",
-        "improvement_surcharge",
-        "total_amount",
-        "payment_type",
-        "trip_type",
-        "pickup",
-        "dropoff",
-        "cab_type",
-        "precipitation",
-        "snow_depth",
-        "snowfall",
-        "max_temperature",
-        "min_temperature",
-        "average_wind_speed",
-        "pickup_nyct2010_gid",
-        "pickup_ctlabel",
-        "pickup_borocode",
-        "pickup_boroname",
-        "pickup_ct2010",
-        "pickup_boroct2010",
-        "pickup_cdeligibil",
-        "pickup_ntacode",
-        "pickup_ntaname",
-        "pickup_puma",
-        "dropoff_nyct2010_gid",
-        "dropoff_ctlabel",
-        "dropoff_borocode",
-        "dropoff_boroname",
-        "dropoff_ct2010",
-        "dropoff_boroct2010",
-        "dropoff_cdeligibil",
-        "dropoff_ntacode",
-        "dropoff_ntaname",
-        "dropoff_puma",
-    ]
-
-    columns_types = [
-        "int64",
-        "category",
-        "timestamp",
-        "timestamp",
-        "category",
-        "int64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "int64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "category",
-        "float64",
-        "category",
-        "category",
-        "category",
-        "float64",
-        "int64",
-        "float64",
-        "int64",
-        "int64",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "category",
-        "float64",
-        "float64",
-        "category",
-        "category",
-        "category",
-        "float64",
-        "float64",
-        "float64",
-        "float64",
-        "category",
-        "float64",
-        "float64",
-        "category",
-        "category",
-        "category",
-        "float64",
-    ]
-
     if parameters["dfiles_num"] <= 0:
         raise ValueError(f"Bad number of data files specified: {parameters['dfiles_num']}")
 
@@ -346,18 +324,11 @@ def run_benchmark(parameters):
     n_files = parameters["dfiles_num"]
     dataset_root = Path(parameters["data_file"])
     filename = [str(dataset_root / s) for s in islice(ny_taxi_data_files_sizes_MB, n_files)]
-    results = etl(
+    etl(
         filename=filename,
-        files_limit=n_files,
         columns_names=columns_names,
         columns_types=columns_types,
         output_for_validation=pd_queries_outputs,
-        backend=parameters["backend"],
-    )
-
-    print_results(results=results, backend=parameters["backend"])
-    return BenchmarkResults(
-        results, params={"dataset_size": get_ny_taxi_dataset_size(parameters["dfiles_num"])}
     )
 
 
@@ -380,7 +351,15 @@ class Benchmark(BaseBenchmark):
         )
 
     def run_benchmark(self, params) -> BenchmarkResults:
-        return run_benchmark(params)
+        with tm.timeit("total"):
+            run_benchmark(params)
+
+        task2time = tm.get_results()
+        print_results(task2time)
+
+        return BenchmarkResults(
+            task2time, params={"dataset_size": get_ny_taxi_dataset_size(params["dfiles_num"])}
+        )
 
     def load_data(self, target_dir, reload=False):
         from timedf.tools.s3_load import download_folder
